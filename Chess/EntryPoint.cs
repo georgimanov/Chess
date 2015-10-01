719f5770d642d9578fe84728f4283f3b8881d9b3
georgimanov <georgimanov@gmail.com>
2015-10-01 13:05:34 +0300

    time saved

--- Chess/EntryPoint.cs
+++ Chess/EntryPoint.cs
@@ -5,7 +5,7 @@
         public static void Main()
         {
             // https://www.youtube.com/watch?v=goBgD_f7-6o
-            // Time: 3:55:06
+            // Time: 4:17:07
             ChessFacade.Start();
         }
     }
